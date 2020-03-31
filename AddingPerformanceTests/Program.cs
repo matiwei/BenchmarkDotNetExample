3ea5c8f8ed97d577caca3157523887f53ba7af8e
Mateusz Weigle <matiwei@gmail.com>
2020-03-31 15:55:38 +0200

    Added project with examples

---
+++ AddingPerformanceTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace AddingPerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SumPerformanceBenchmarks>();
+        }
+    }
+}
---
+++ AddingPerformanceTests/SumPerformanceTestFunctionsList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddingPerformanceTests
+{
+    public class SumPerformanceTestFunctionsList
+    {
+        public static int ListSumEqualsAPlusB(List<int> intList, int startIndex, int endIndex)
+        {
+            int sum = 0;
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                sum = sum + intList[i];
+            }
+            return sum;
+        }
+
+        public static int ListSumPlusEquals(List<int> intList, int startIndex, int endIndex)
+        {
+            int sum = 0;
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                sum += intList[i];
+            }
+            return sum;
+        }
+
+        public static int ListSumForeach(List<int> intList, int startIndex, int endIndex)
+        {
+            int sum = 0;
+            foreach (int i in intList.GetRange(startIndex, endIndex - startIndex))
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
